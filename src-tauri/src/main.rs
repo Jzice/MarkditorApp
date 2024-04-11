e5101d04ea85a66d5cfdf8e6e5dce97bbe51e672
Justice <justice_103@126.com>
2024-04-11 14:13:17 +0800

    Mod dark theme background transparent

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,32 @@ use crate::utils::set_window_shadow;
 mod fs_api;
 mod utils;
 
+use tauri::{App, Manager};
+use window_vibrancy::{self, NSVisualEffectMaterial};
+
+/// setup
+pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let win = app.get_window("main").unwrap();
+
+    // 仅在 macOS 下执行
+    #[cfg(target_os = "macos")]
+    window_vibrancy::apply_vibrancy(&win, NSVisualEffectMaterial::FullScreenUI)
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+    // 仅在 windows 下执行
+    #[cfg(target_os = "windows")]
+    window_vibrancy::apply_mica(&win, Some(true))
+        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+    set_window_shadow(app);
+
+    Ok(())
+}
+
+
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            set_window_shadow(app);
-            Ok(())
-        })
+        .setup(init)
         .invoke_handler(tauri::generate_handler![fs_api::rename_dir]) /* add event handlers here */
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
